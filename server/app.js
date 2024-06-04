c8ea759ede7961a41864bf1a0375a3904e4cc3a1
Duncan <99960161+DuncanGuijo@users.noreply.github.com>
2024-06-04 21:13:22 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,19 +4,10 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json())
-//Esto creo que deberia cribarse bien pero de momento lo dejamos asi
-//Aunque podemos ser más específicos de la siguiente manera:
-//app.use(cors({
-//  origin: 'http://localhost:3000'
-//}));
+
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Enruta todas las solicitudes a la aplicación de React
-/* app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-}); */
 
 //Routes
 const productsRouter = require("./routes/Products")
